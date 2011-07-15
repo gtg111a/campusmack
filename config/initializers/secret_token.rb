9dc7154e995651bbaae6b89e0fcaec62427b926d
Michael Bivone <michael.bivone@gmail.com>
2011-07-15 09:43:00 -0400

    add all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Campusmack::Application.config.secret_token = 'd52b5c66c3bc8b04a77327c03b7cd6955b806e6790874ae95eab55e4400e99926fd0ea69c1fb8b039f294072bc4e1c81606716da17131806759ce7d85a8feece'
