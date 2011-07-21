b7f26e88412bce016a4f60b02aef1094ab6d2618
Michael Bivone <michael.bivone@gmail.com>
2011-07-21 13:48:23 -0400

    environment.rb issue

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Campusmack::Application.initialize!
 
-Rails::Initializer.run do |config|
-  config.action_controller.default_charset = "utf-8"
-end
+#Rails::Initializer.run do |config|
+#  config.action_controller.default_charset = "utf-8"
+#end
 
