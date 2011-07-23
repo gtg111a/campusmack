095afe9315eef51b7c51809428db6c4b45da83a4
Michael Bivone <michael.bivone@gmail.com>
2011-07-23 10:56:34 -0400

    rm more files

--- db/migrate/20110719232250_remove_type_from_smacks.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTypeFromSmacks < ActiveRecord::Migration
-  def self.up
-    remove_column :smacks, :type
-  end
-
-  def self.down
-    add_column :smacks, :type, :string
-  end
-end
--- db/migrate/20110721232318_remove_type_from_redemptions.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTypeFromRedemptions < ActiveRecord::Migration
-  def self.up
-    remove_column :redemptions, :photo_post
-  end
-
-  def self.down
-    add_column :redemptions, :photo_post, :string
-  end
-end
