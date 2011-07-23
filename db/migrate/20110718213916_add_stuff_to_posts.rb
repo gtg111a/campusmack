538535cfe6eb00ed14430b1ca007b6906aa355ec
Michael Bivone <michael.bivone@gmail.com>
2011-07-23 10:50:26 -0400

    removed files

--- db/migrate/20110718213916_add_stuff_to_posts.rb
+++
@@ -1,9 +0,0 @@
-class AddStuffToPosts < ActiveRecord::Migration
-  def self.up
-    add_column :posts, :college_id, :integer
-  end
-
-  def self.down
-    remove_column :posts, :college_id, :integer
-  end
-end
