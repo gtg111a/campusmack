2c1974696f4e73f2cc00e074ad0599b6168eb57f
arpadlukacs <arpad.lukacs@gmail.com>
2011-08-30 12:32:48 +0200

    Fixed unmerged files.

--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -1,15 +1,6 @@
 class SupportController < ApplicationController
-<<<<<<< HEAD
-  skip_authorization_check
-
-    def new
-      # id is required to deal with form
-      @support = Support.new(:id => 1)
-    end
-=======
   skip_authorization_check :only => [:new, :create]
   load_and_authorize_resource
->>>>>>> b79f58bb0cdc89a0ad3d39eb72adeecf4d520801
 
   def new
     # id is required to deal with form
