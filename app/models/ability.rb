b2dd9ea4403a51639c357cfce3e181adb22f49c4
Michael Bivone <michael.bivone@gmail.com>
2011-08-30 09:54:01 -0400

    updates

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -16,6 +16,7 @@ class Ability
       can [:update, :destroy], Smack, :user_id => user.id
       can [:vote_up, :vote_down], Post
       can [:manage], Authentication, :user_id => user.id
+      can :manage, Vote, :user_id => user.id
     end
     can :create, Support
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,37 +1,42 @@
 class Post < ActiveRecord::Base
-  
-   include PostsHelper  
+
+  include PostsHelper
   #Through the 'make_voteable' gem
   #make_voteable
-  
+
   #Through the 'thumbs_up' gem
   acts_as_voteable
-  
+
   #Through the 'acts_as_commentable' gem
   acts_as_commentable
-  
+
   attr_accessible :content_type, :title, :content, :post_summary, :vote, :photo, :news
-  
+
   attr_searchable :title, :post_summary
-  
+
   belongs_to :college
   belongs_to :user
   has_many :comments, :as => :commentable, :dependent => :destroy
-  
+
   accepts_nested_attributes_for :user
   accepts_nested_attributes_for :college
-  
-  
-  has_attached_file :photo, 
+
+
+  has_attached_file :photo,
                     :styles => {:medium => "200x200", :large => "600x600"},
                     :storage => :s3,
                     :s3_credentials => S3_CREDENTIALS,
                     :bucket => 'Campusmack',
                     :path => "/:style/:id/:filename"
-                 
-  validates :type,          :presence => true
-  validates :content_type,  :presence => true
-  validates :title,         :presence => true
-  
-  
+
+  validates :type, :presence => true
+  validates :content_type, :presence => true
+  validates :title, :presence => true
+
+  scope :videos, :conditions => ["posts.content_type LIKE ?", "Video"]
+  scope :photos, :conditions => ["posts.content_type LIKE ?", "Photo"]
+  scope :smacks, :conditions => ["posts.type LIKE ?", "Smack"]
+  scope :redemptions, :conditions => ["posts.type LIKE ?", "Redemption"]
+  default_scope :order => 'created_at DESC'
+
 end
