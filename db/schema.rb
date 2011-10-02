c489f1e485360a3642f76427152475a864d57ede
Michael Bivone <michael.bivone@gmail.com>
2011-10-02 08:51:48 -0400

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110919155522) do
+ActiveRecord::Schema.define(:version => 20110928131409) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -208,6 +208,7 @@ ActiveRecord::Schema.define(:version => 20110919155522) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "censor_text",                           :default => true
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
