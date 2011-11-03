0ccb6543a9b515d9f8b34fc487f14d5e84bc9ec5
arpadlukacs <arpad.lukacs@gmail.com>
2011-11-03 13:36:42 +0100

    Ensure domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  APP_DOMAIN = 'campusmack.com'
   rescue_from Exception, :with => :render_error
 
   protect_from_forgery
@@ -11,6 +12,7 @@ class ApplicationController < ActionController::Base
   before_filter :init
   before_filter :add_initial_breadcrumbs
   before_filter :get_leftmenu_content
+  before_filter :ensure_domain
 
   rescue_from CanCan::AccessDenied do |exception|
     target_url = signed_in? ? root_url : sign_up_url
@@ -59,6 +61,11 @@ class ApplicationController < ActionController::Base
 
   def log_exception(exception)
     puts "\nEXCEPTION: #{exception}\n--------- BACKTRACE:\n#{exception.backtrace[0..4].join("\n")}\n--------- END OF BACKTRACE"
+  end
+
+  def ensure_domain
+    return if request.local? || request.env['HTTP_HOST'].ends_with?(APP_DOMAIN)
+    redirect_to "http://#{APP_DOMAIN}", :status => 301
   end
 
 end
