604c3a8f836043b6269114d17301b71961e46370
arpadlukacs <arpad.lukacs@gmail.com>
2011-12-20 16:48:11 +0100

    trying to fix a postgres related issue.

--- app/controllers/contest_controller.rb
+++ app/controllers/contest_controller.rb
@@ -1,9 +1,9 @@
 class ContestController < ApplicationController
-  
+
   def index
     @title = "Contest"
     posts = Post.joins(:video)
-    @search = posts.search(params[:search]).where("contest = 1")
+    @search = posts.search(params[:search]).where("contest = ?", true)
     @order = params[:order] || Post::default_order
     @per_page = params[:per] || Post::PER_PAGE_DEFAULT[0]
     @posts = @search.paginate(:page => params[:page], :order => @order, :per_page => @per_page)
@@ -13,5 +13,5 @@ class ContestController < ApplicationController
     # We use the views from the posts folder for everything
     render 'posts/index'
   end
-  
+
 end
