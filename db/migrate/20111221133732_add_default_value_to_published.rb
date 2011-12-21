0c1b4fc6f9428f616d029221e6de3722c23bf9da
arpadlukacs <arpad.lukacs@gmail.com>
2011-12-21 15:23:00 +0100

    pg fix

--- db/migrate/20111221133732_add_default_value_to_published.rb
+++ db/migrate/20111221133732_add_default_value_to_published.rb
@@ -1,8 +1,8 @@
 class AddDefaultValueToPublished < ActiveRecord::Migration
   def self.up
     change_column_default(:posts, :published, :true)
-    Post.update_all([ 'published = ?', true ], 'published IS NULL AND type != "ArticlePost"')
-    Post.update_all([ 'published = ?', false ], 'published IS NULL AND type = "ArticlePost"')
+    Post.update_all([ 'published = ?', true ], [ 'published IS NULL AND type != ?', "ArticlePost" ])
+    Post.update_all([ 'published = ?', false ], [ 'published IS NULL AND type = ?', "ArticlePost" ])
   end
 
   def self.down
