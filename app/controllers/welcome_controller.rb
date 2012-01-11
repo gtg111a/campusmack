f9ab3331043d4ebf89b50e7fe9b5341b66ae41ed
Arpad Lukacs <arpad.lukacs@gmail.com>
2012-01-11 19:12:29 +0100

    Merged design work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     @colleges = College.all(:order => "smacks_count DESC", :limit => 10)
+    @featured_articles = Post.where(:type => 'ArticlePost').limit(4)
   end
 
   def change_division
