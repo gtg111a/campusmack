aa3ab5f6e6ae1958e2b84a8fb0ca1bcc0a46a022
Michael Bivone <michael.bivone@gmail.com>
2012-02-07 21:59:16 -0500

    About Update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,18 +3,18 @@ class PagesController < ApplicationController
 
   def about
     @menu = [
+      [:text, 'About', '', 'active'],
       [:link, 'FAQ', '/faq'],
-      [:link, 'How To', '/howto'],
-      [:link, 'Our Team','/ourteam']
+      [:link, 'How To', '/howto']
     ]
     breadcrumbs.add 'About'
   end
 
   def faq
     @menu = [
+      [:link, 'About', '/about'],
       [:text, 'FAQ', '', 'active'],
-      [:link, 'How To', '/howto'],
-      [:link, 'Our Team', '/ourteam']
+      [:link, 'How To', '/howto']
     ]
     breadcrumbs.add 'About', '/about'
     breadcrumbs.add 'FAQ'
@@ -22,23 +22,13 @@ class PagesController < ApplicationController
 
   def howto
     @menu = [
+      [:link, 'About', '/about'],
       [:link, 'FAQ', '/faq'],
-      [:text, 'How To', '', 'active'],
-      [:link, 'Our Team', '/ourteam']
+      [:text, 'How To', '', 'active']   
     ]
     breadcrumbs.add 'About', '/about'
     breadcrumbs.add 'How To'
   end
-  
-  def ourteam
-     @menu = [
-       [:link, 'FAQ', '/faq'],
-       [:link, 'How To', '/howto'],
-       [:text, 'Our Team', '', 'active']
-     ]
-     breadcrumbs.add 'About', '/about'
-     breadcrumbs.add 'Our Team'
-   end
   
 
   def help
