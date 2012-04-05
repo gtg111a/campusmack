e529e5b065f7367e418f03927158b0e2febb77ff
Arpad Lukacs <arpad.lukacs@gmail.com>
2012-04-05 12:24:15 +0200

    Changed contact groups method

--- app/models/contact_groups_contact.rb
+++ app/models/contact_groups_contact.rb
@@ -4,12 +4,12 @@ class ContactGroupsContact < ActiveRecord::Base
 
   validates :contact_id, :uniqueness => {:scope => :contact_group_id}
 
-  def self.add_group_contacts(group, contact_ids)
+  def self.add_group_contacts(group_id, contact_ids)
+    group = ContactGroup.find(group_id)
     if group.present? && !group.new_record?
       if contact_ids != nil
         contacts_saved_in_group = []
-        cb = contact_ids.split(',')
-        cb.each do |contact_id|
+        contact_ids.each do |contact_id|
           contacts_in_group = ContactGroupsContact.new(:contact_id => contact_id.to_i ,:contact_group_id => group.id)
           begin
             contacts_in_group.save!
