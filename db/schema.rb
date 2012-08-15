3f535720dfbd3f97b84a429e054d71270ec5bb87
Michael Bivone <michael.bivone@gmail.com>
2012-08-15 14:35:59 -0400

    Added Permalink to ArticlePosts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120117131307) do
+ActiveRecord::Schema.define(:version => 20120815180735) do
 
   create_table "articles", :force => true do |t|
     t.integer   "post_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20120117131307) do
     t.timestamp "image_updated_at"
     t.timestamp "created_at"
     t.timestamp "updated_at"
+    t.string    "permalink"
   end
 
   create_table "authentications", :force => true do |t|
@@ -63,14 +64,14 @@ ActiveRecord::Schema.define(:version => 20120117131307) do
   add_index "colleges", ["permalink"], :name => "index_colleges_on_permalink"
 
   create_table "comments", :force => true do |t|
-    t.string    "title",            :limit => 50, :default => ""
+    t.string    "title",            :limit => 50
     t.text      "comment"
     t.integer   "commentable_id"
     t.string    "commentable_type"
     t.integer   "user_id"
     t.timestamp "created_at"
     t.timestamp "updated_at"
-    t.integer   "reports_count",                  :default => 0,  :null => false
+    t.integer   "reports_count",                  :default => 0, :null => false
   end
 
   add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
@@ -127,10 +128,11 @@ ActiveRecord::Schema.define(:version => 20120117131307) do
     t.timestamp "updated_at"
   end
 
-  create_table "maintenances", :force => true do |t|
-    t.string   "task"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "microposts", :force => true do |t|
+    t.string    "content"
+    t.integer   "user_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "news_posts", :force => true do |t|
@@ -157,22 +159,24 @@ ActiveRecord::Schema.define(:version => 20120117131307) do
   end
 
   create_table "posts", :force => true do |t|
-    t.string   "title"
-    t.text     "summary",           :limit => 255
-    t.string   "type"
-    t.boolean  "published",                        :default => true
-    t.integer  "postable_id"
-    t.string   "postable_type"
-    t.integer  "user_id"
-    t.integer  "on_frontpage_week"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "reports_count",                    :default => 0,     :null => false
-    t.integer  "up_votes",                         :default => 0,     :null => false
-    t.integer  "down_votes",                       :default => 0,     :null => false
-    t.boolean  "contest",                          :default => false
+    t.string    "title"
+    t.text      "summary"
+    t.string    "type"
+    t.boolean   "published",         :default => true
+    t.integer   "postable_id"
+    t.string    "postable_type"
+    t.integer   "user_id"
+    t.integer   "on_frontpage_week"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.integer   "reports_count",     :default => 0,     :null => false
+    t.integer   "up_votes",          :default => 0,     :null => false
+    t.integer   "down_votes",        :default => 0,     :null => false
+    t.boolean   "contest",           :default => false
+    t.string    "permalink"
   end
 
+  add_index "posts", ["permalink"], :name => "index_posts_on_permalink", :unique => true
   add_index "posts", ["postable_id"], :name => "index_posts_on_postable_id"
   add_index "posts", ["postable_type"], :name => "index_posts_on_postable_type"
   add_index "posts", ["type"], :name => "index_posts_on_type"
@@ -218,7 +222,7 @@ ActiveRecord::Schema.define(:version => 20120117131307) do
   end
 
   add_index "relationships", ["followed_id"], :name => "index_relationships_on_followed_id"
-  add_index "relationships", ["follower_id", "followed_id"], :name => "index_relationships_on_follower_id_and_followed_id", :unique => true
+  add_index "relationships", ["follower_id", "followed_id"], :name => "index_relationships_on_follower_id_and_followed_id"
   add_index "relationships", ["follower_id"], :name => "index_relationships_on_follower_id"
 
   create_table "reports", :force => true do |t|
@@ -255,6 +259,14 @@ ActiveRecord::Schema.define(:version => 20120117131307) do
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
+  create_table "smack_sends", :force => true do |t|
+    t.integer   "user_id"
+    t.integer   "post_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.integer   "send_number"
+  end
+
   create_table "smacks", :force => true do |t|
     t.timestamp "created_at"
     t.timestamp "updated_at"
@@ -278,14 +290,14 @@ ActiveRecord::Schema.define(:version => 20120117131307) do
 
   create_table "users", :force => true do |t|
     t.string    "username"
-    t.string    "email",                                 :default => "",     :null => false
+    t.string    "email",                                                     :null => false
     t.string    "first_name"
     t.string    "last_name"
     t.integer   "college_id"
     t.string    "affiliation"
     t.integer   "up_votes"
     t.integer   "down_votes"
-    t.string    "encrypted_password",     :limit => 128, :default => "",     :null => false
+    t.string    "encrypted_password",     :limit => 128,                     :null => false
     t.string    "reset_password_token"
     t.timestamp "reset_password_sent_at"
     t.string    "confirmation_token"
@@ -311,8 +323,8 @@ ActiveRecord::Schema.define(:version => 20120117131307) do
     t.string    "role",                                  :default => "user"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token"
   add_index "users", ["username"], :name => "index_users_on_username"
 
   create_table "videos", :force => true do |t|
@@ -333,7 +345,7 @@ ActiveRecord::Schema.define(:version => 20120117131307) do
   end
 
   add_index "votes", ["voteable_id", "voteable_type"], :name => "index_votes_on_voteable_id_and_voteable_type"
-  add_index "votes", ["voter_id", "voter_type", "voteable_id", "voteable_type"], :name => "fk_one_vote_per_user_per_entity", :unique => true
+  add_index "votes", ["voter_id", "voter_type", "voteable_id", "voteable_type"], :name => "fk_one_vote_per_user_per_entity"
   add_index "votes", ["voter_id", "voter_type"], :name => "index_votes_on_voter_id_and_voter_type"
 
   create_table "votings", :force => true do |t|
